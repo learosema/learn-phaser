77d835e3a6ee3be3127897109e7c9d8354424ffe
Lea Rosema <terabaud@gmail.com>
2019-05-05 15:12:11 +0200

    add first-game

--- README.md
+++ README.md
@@ -7,6 +7,7 @@ USAGE: just `npm start` to start a [local webserver](http://localhost:8000/).
 Alternatively, this repo is also pushed as a [github page](https://terabaud.github.io/learn-phaser/)
 
  * [hello-phaser](hello-phaser/) by [Phaser official documentation](https://phaser.io/tutorials/getting-started-phaser3/part5)
+ * [first-game](first-game/) by [Phaser first game tutorial](http://phaser.io/tutorials/making-your-first-phaser-3-game/part1)
  * [infinite map](infinite-map/) by Jared York ([article](https://yorkcs.com/2019/02/25/top-down-infinite-terrain-generation-with-phaser-3/))
 
 
---
+++ first-game/index.js
@@ -0,0 +1,151 @@
+let player;
+let platforms;
+let stars;
+let bombs;
+let cursors;
+let scoreText;
+let score = 0;
+let gameOver = false;
+
+const config = {
+  type: Phaser.AUTO,
+  parent: 'phaser-example',
+  mode: Phaser.Scale.FIT,
+  autoCenter: Phaser.Scale.CENTER_BOTH,
+  width: 800,
+  height: 600,
+  physics: {
+    default: 'arcade',
+    arcade: {
+      gravity: { y: 300 },
+      debug: false
+    }
+  },
+  scene: {
+    preload: preload,
+    create: create,
+    update: update
+  }
+};
+
+function preload() {
+  this.load.image('sky', 'assets/sky.png');
+  this.load.image('ground', 'assets/platform.png');
+  this.load.image('star', 'assets/star.png');
+  this.load.image('bomb', 'assets/bomb.png');
+  this.load.spritesheet('dude', 'assets/dude.png', {
+    frameWidth: 32,
+    frameHeight: 48
+  });
+}
+
+function create() {
+  this.add.image(400, 300, 'sky');
+  scoreText = this.add.text(16, 16, 'score: 0', {
+    fontSize: '32px',
+    fill: '#000'
+  });
+  platforms = this.physics.add.staticGroup();
+  platforms
+    .create(400, 568, 'ground')
+    .setScale(2)
+    .refreshBody();
+  platforms.create(600, 400, 'ground');
+  platforms.create(50, 250, 'ground');
+  platforms.create(750, 220, 'ground');
+
+  player = this.physics.add.sprite(100, 450, 'dude');
+  cursors = this.input.keyboard.createCursorKeys();
+  player.setBounce(0.2);
+  player.setCollideWorldBounds(true);
+  // player.body.gravity.set(0, 300);
+  this.physics.add.collider(player, platforms);
+
+  this.anims.create({
+    key: 'left',
+    frames: this.anims.generateFrameNumbers('dude', { start: 0, end: 3 }),
+    frameRate: 10,
+    repeat: -1
+  });
+
+  this.anims.create({
+    key: 'turn',
+    frames: [{ key: 'dude', frame: 4 }],
+    frameRate: 20
+  });
+
+  this.anims.create({
+    key: 'right',
+    frames: this.anims.generateFrameNumbers('dude', { start: 5, end: 8 }),
+    frameRate: 10,
+    repeat: -1
+  });
+
+  stars = this.physics.add.group({
+    key: 'star',
+    repeat: 11,
+    setXY: { x: 12, y: 0, stepX: 70 }
+  });
+
+  stars.children.iterate(function(child) {
+    child.setBounceY(
+      Phaser.Math.FloatBetween(0.4, 0.8)
+    );
+  });
+  this.physics.add.collider(stars, platforms);
+  this.physics.add.overlap(player, stars, collectStar, undefined, this);
+
+  bombs = this.physics.add.group();
+  this.physics.add.collider(bombs, platforms);
+  this.physics.add.collider(player, bombs, hitBomb, undefined, this);
+}
+
+function hitBomb(player, bomb) {
+  this.physics.pause();
+  player.setTint(0xff0000);
+  player.anims.play('turn');
+  gameOver = true;
+}
+
+function collectStar(player, star) {
+  star.disableBody(true, true);
+  score += 10;
+  scoreText.setText('Score: ' + score);
+
+  if (stars.countActive(true) === 0) {
+    stars.children.iterate(function(child) {
+      child.enableBody(true, child.x, 0, true, true);
+    });
+    var x =
+      player.x < 400
+        ? Phaser.Math.Between(400, 800)
+        : Phaser.Math.Between(0, 400);
+    var bomb = bombs.create(x, 16, 'bomb');
+    bomb.setBounce(1);
+    bomb.setCollideWorldBounds(true);
+    bomb.setVelocity(Phaser.Math.Between(-200, 200), 20);
+  }
+}
+
+function update() {
+  if (gameOver) {
+    return;
+  }
+  if (cursors.left && cursors.left.isDown) {
+    player.setVelocityX(-160);
+    player.anims.play('left', true);
+  } else if (cursors.right && cursors.right.isDown) {
+    player.setVelocityX(160);
+
+    player.anims.play('right', true);
+  } else {
+    player.setVelocityX(0);
+    player.anims.play('turn');
+  }
+
+  if (cursors.up && cursors.up.isDown && player.body.touching.down) {
+    player.setVelocityY(-330);
+  }
+}
+
+const myGame = new Phaser.Game(config);
